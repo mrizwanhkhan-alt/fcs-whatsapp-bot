0de20eac9bb8280114d2af09363372c54e542b4c
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-07-30 19:28:27 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+module.exports = {
+  PORT: Number(process.env.PORT) || 10000,
 
+  VERIFY_TOKEN:
+    (process.env.VERIFY_TOKEN || "FCS2026Verify").trim(),
+
+  PHONE_NUMBER_ID:
+    (process.env.PHONE_NUMBER_ID || "").trim(),
+
+  WHATSAPP_TOKEN:
+    (process.env.WHATSAPP_TOKEN || "").trim(),
+
+  OPENAI_API_KEY:
+    (process.env.OPENAI_API_KEY || "").trim()
+};
