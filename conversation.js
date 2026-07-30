ebb24726d7a15d4251f2113bc48603c1eaec85e4
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-07-30 19:07:34 +0400

    Update conversation.js

--- conversation.js
+++ conversation.js
@@ -1,1 +1,19 @@
+const conversations = new Map();
 
+function get(number) {
+  return conversations.get(number);
+}
+
+function set(number, data) {
+  conversations.set(number, data);
+}
+
+function remove(number) {
+  conversations.delete(number);
+}
+
+module.exports = {
+  get,
+  set,
+  remove
+};
