62522f0577de29b8c3215d23aa4bad1fd431607b
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-07-31 15:02:00 +0400

    Update applicationHandler.js

--- applicationHandler.js
+++ applicationHandler.js
@@ -1,9 +1,25 @@
 const { applications, questions } = require("./application");
 
+const fields = [
+  "fullName",
+  "fatherName",
+  "mobile",
+  "whatsapp",
+  "email",
+  "province",
+  "city",
+  "area",
+  "address",
+  "education",
+  "experience",
+  "shop",
+  "comments"
+];
+
 function startApplication(number) {
   applications.set(number, {
     step: 0,
-    answers: {}
+    data: {}
   });
 
   return (
@@ -17,40 +33,30 @@ function isApplying(number) {
   return applications.has(number);
 }
 
-function handleApplication(number, message) {
+function handleApplication(number, answer) {
+
   const app = applications.get(number);
 
   if (!app) {
-    return null;
+    return {
+      completed: false,
+      reply: "Application not found."
+    };
   }
 
-  const fields = [
-    "fullName",
-    "fatherName",
-    "mobile",
-    "whatsapp",
-    "email",
-    "province",
-    "city",
-    "area",
-    "address",
-    "education",
-    "experience",
-    "shop",
-    "comments"
-  ];
+  app.data[fields[app.step]] = answer;
 
-  app.answers[fields[app.step]] = message;
   app.step++;
 
   if (app.step >= questions.length) {
-    const completed = app.answers;
+
+    const completedData = app.data;
 
     applications.delete(number);
 
     return {
       completed: true,
-      data: completed,
+      data: completedData,
       reply:
 `🎉 Thank you!
 
@@ -58,6 +64,7 @@ Your franchise application has been submitted successfully.
 
 Our Franchise Development Team will review your application and contact you shortly.`
     };
+
   }
 
   applications.set(number, app);
