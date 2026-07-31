068ee24631e9b4d3c75e03e2cf7ec4ecdb86439b
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-07-31 15:00:24 +0400

    Update application.js

--- application.js
+++ application.js
@@ -1,1 +1,22 @@
+const applications = new Map();
 
+const questions = [
+  "What is your Full Name?",
+  "What is your Father's Name?",
+  "What is your Mobile Number?",
+  "What is your WhatsApp Number? (Type 'Same' if it is the same.)",
+  "What is your Email Address? (Optional)",
+  "Which Province are you applying from?",
+  "Which City?",
+  "Which Area / Tehsil?",
+  "What is your Office / Shop Address?",
+  "What is your Education?",
+  "Please describe your Business Experience.",
+  "Do you already have a shop or office? (Yes / No)",
+  "Any additional comments? (Optional)"
+];
+
+module.exports = {
+  applications,
+  questions
+};
