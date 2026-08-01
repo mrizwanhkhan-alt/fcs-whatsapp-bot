9c329e6b644208517572787b40fc427f6d02795e
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-01 16:37:03 +0400

    Update engagementManager.js

--- engagementManager.js
+++ engagementManager.js
@@ -2,32 +2,49 @@ const { engagementPlan } = require("./engagement");
 
 
 // Calculate applicant waiting days
+
 function getDaysPassed(applicationDate) {
 
   const today = new Date();
 
   const appliedDate = new Date(applicationDate);
 
+
   const difference =
     today - appliedDate;
 
+
   const days =
-    Math.floor(difference / (1000 * 60 * 60 * 24));
+    Math.floor(
+      difference /
+      (1000 * 60 * 60 * 24)
+    );
+
 
   return days;
 
 }
 
 
-// Find correct engagement message
-function getEngagementMessage(applicationDate) {
 
-  const daysPassed = getDaysPassed(applicationDate);
+// Find correct engagement message
+
+function getEngagementMessage(
+  applicationDate,
+  engagementStage
+) {
+
+
+  const daysPassed =
+    getDaysPassed(applicationDate);
+
 
   let selectedMessage = null;
 
 
+
   for (const item of engagementPlan) {
+
 
     if (daysPassed >= item.day) {
 
@@ -38,12 +55,46 @@ function getEngagementMessage(applicationDate) {
   }
 
 
-  return selectedMessage;
+
+  if (!selectedMessage) {
+
+    return null;
+
+  }
+
+
+
+  // Avoid sending same stage again
+
+  if (
+    engagementStage === selectedMessage.stage
+  ) {
+
+    return null;
+
+  }
+
+
+
+  return {
+
+    stage: selectedMessage.stage,
+
+    message: selectedMessage.message,
+
+    nextDay: selectedMessage.nextDay
+
+  };
+
 
 }
 
 
+
 module.exports = {
+
   getDaysPassed,
+
   getEngagementMessage
+
 };
