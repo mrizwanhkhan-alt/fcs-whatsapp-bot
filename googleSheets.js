a7085f8a3257f92737cda9105d25fce23367d8f9
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-01 10:17:31 +0400

    Update googleSheets.js

--- googleSheets.js
+++ googleSheets.js
@@ -12,6 +12,17 @@ const sheets = google.sheets({
   auth
 });
 
+async function appendApplication(row) {
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: config.GOOGLE_SHEET_ID,
+    range: "A:N",
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [row]
+    }
+  });
+}
+
 module.exports = {
   sheets
 };
