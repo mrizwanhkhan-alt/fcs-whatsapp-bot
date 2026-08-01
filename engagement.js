ff51c8fe9eee5c8d94025780d02682d895f3091c
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-01 15:02:02 +0400

    Create engagement.js

---
+++ engagement.js
@@ -0,0 +1,29 @@
+const engagementPlan = [
+
+  {
+    day: 0,
+    stage: "Application Submitted",
+    message:
+      "Dear Applicant, your FCS Express Franchise Application has been received successfully and is under process."
+  },
+
+  {
+    day: 15,
+    stage: "Waiting for Review",
+    message:
+      "Dear Applicant, your FCS Express Franchise Application is still under process. We appreciate your patience."
+  },
+
+  {
+    day: 30,
+    stage: "Follow Up",
+    message:
+      "Dear Applicant, we are continuing to review your application. Thank you for your interest in joining FCS Express."
+  }
+
+];
+
+
+module.exports = {
+  engagementPlan
+};
