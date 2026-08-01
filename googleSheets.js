c1758040198f4db123af26022feb10b7ec930f27
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-01 10:13:45 +0400

    Update googleSheets.js

--- googleSheets.js
+++ googleSheets.js
@@ -1,3 +1,17 @@
 const { google } = require("googleapis");
+const config = require("./config");
+const serviceAccount = require("./service-account.json");
 
-module.exports = {};
+const auth = new google.auth.GoogleAuth({
+  credentials: serviceAccount,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+});
+
+const sheets = google.sheets({
+  version: "v4",
+  auth
+});
+
+module.exports = {
+  sheets
+};
