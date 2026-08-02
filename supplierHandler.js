8583a8e355e48ce85781650619e30fc5b213db5a
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-02 12:11:15 +0400

    Create supplierHandler.js

---
+++ supplierHandler.js
@@ -0,0 +1,131 @@
+const {
+  suppliers,
+  supplierQuestions
+} = require("./supplier");
+
+
+const supplierFields = [
+  "companyName",
+  "contactPerson",
+  "mobile",
+  "email",
+  "city",
+  "products",
+  "address",
+  "comments"
+];
+
+
+
+// START SUPPLIER REGISTRATION
+
+function startSupplier(number, lang = "en") {
+
+  suppliers.set(number, {
+    step: 0,
+    data: {},
+    lang: lang
+  });
+
+
+  return supplierQuestions[lang][0];
+
+}
+
+
+
+// CHECK ACTIVE SUPPLIER
+
+function isSupplierRegistering(number) {
+
+  return suppliers.has(number);
+
+}
+
+
+
+// HANDLE SUPPLIER REGISTRATION
+
+function handleSupplier(number, answer) {
+
+
+  const supplier = suppliers.get(number);
+
+
+  if (!supplier) {
+
+    return {
+      completed: false,
+      reply: "Supplier registration not found."
+    };
+
+  }
+
+
+
+  const field = supplierFields[supplier.step];
+
+
+  supplier.data[field] = answer;
+
+
+  supplier.step++;
+
+
+
+  if (supplier.step >= supplierQuestions[supplier.lang].length) {
+
+
+    const data = supplier.data;
+
+
+    suppliers.delete(number);
+
+
+
+    return {
+
+      completed: true,
+
+      reply:
+
+`✅ Thank you!
+
+Your Supplier Registration has been received.
+
+Company: ${data.companyName}
+
+Our team will review your details and contact you.`
+
+    };
+
+  }
+
+
+
+  suppliers.set(number, supplier);
+
+
+
+  return {
+
+    completed: false,
+
+    reply: supplierQuestions[supplier.lang][supplier.step]
+
+  };
+
+
+}
+
+
+
+module.exports = {
+
+  startSupplier,
+
+  isSupplierRegistering,
+
+  handleSupplier
+
+};
