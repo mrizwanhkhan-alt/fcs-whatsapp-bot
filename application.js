92df46c08beb58d824624065d887a19073e18b26
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-06 17:11:14 +0400

    Update application.js

--- application.js
+++ application.js
@@ -24,10 +24,8 @@ const questions = {
 
     "What is your Office / Shop Address?",
 
-    "What is your Education?",
-
-    "Please describe your Business Experience.",
-
+  "What is your Education?\n\n1. Primary\n2. Middle\n3. Matric\n4. Intermediate\n5. Graduate\n6. Masters",
+  "Please select your Business Experience:\n\n1. No Experience\n2. Less than 1 Year\n3. 1-3 Years\n4. 3-5 Years\n5. More than 5 Years",
     "Do you already have a shop or office? (Yes / No)",
 
     "Any additional comments? (Optional)"
@@ -56,10 +54,8 @@ const questions = {
 
     "آپ کے دفتر / دکان کا مکمل پتہ کیا ہے؟",
 
-    "آپ کی تعلیم کیا ہے؟",
-
-    "اپنے کاروباری تجربے کے بارے میں بتائیں۔",
-
+ "آپ کی تعلیم کیا ہے؟\n\n1. پرائمری\n2. مڈل\n3. میٹرک\n4. انٹرمیڈیٹ\n5. گریجویٹ\n6. ماسٹرز",
+   "اپنے کاروباری تجربے کا انتخاب کریں:\n\n1. کوئی تجربہ نہیں\n2. 1 سال سے کم\n3. 1 سے 3 سال\n4. 3 سے 5 سال\n5. 5 سال سے زیادہ",
     "کیا آپ کے پاس پہلے سے دکان یا دفتر موجود ہے؟ (ہاں / نہیں)",
 
     "اگر کوئی اضافی معلومات دینا چاہتے ہیں تو لکھیں۔ (اختیاری)"
