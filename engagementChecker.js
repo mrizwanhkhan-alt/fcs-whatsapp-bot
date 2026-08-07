1c764b55b8368b6676b96ef3dbd4b486fc4f8032
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-07 19:42:38 +0400

    Update engagementChecker.js

--- engagementChecker.js
+++ engagementChecker.js
@@ -1,25 +1,35 @@
 const {
   getApplications,
-  updateEngagement
+  getSuppliers,
+  getTransports,
+  getWarehouses,
+  updateEngagement,
+  updateSupplierEngagement,
+  updateTransportEngagement,
+  updateWarehouseEngagement
 } = require("./googleSheets");
 
 const {
   getEngagementMessage
 } = require("./engagementManager");
 
-async function checkEngagement(sendWhatsAppMessage) {
-
-  const rows = await getApplications();
-
+async function processRows(
+  rows,
+  mobileColumn,
+  statusColumn,
+  stageColumn,
+  updateFunction,
+  sendEngagementMessage
+) {
   for (let i = 1; i < rows.length; i++) {
 
     const row = rows[i];
     const sheetRow = i + 1;
 
     const applicationDate = row[0];
-    const whatsappNumber = row[5];
-    const status = row[16];
-    const engagementStage = row[17];
+    const whatsappNumber = row[mobileColumn];
+    const status = row[statusColumn];
+    const engagementStage = row[stageColumn];
 
     if (
       status === "Approved" ||
@@ -39,24 +49,72 @@ async function checkEngagement(sendWhatsAppMessage) {
       continue;
     }
 
-    await sendWhatsAppMessage(
+    await sendEngagementMessage(
       whatsappNumber,
-      engagement.message
+      engagement
     );
 
-    const nextDate = new Date();
+    let nextDate = "";
 
-    nextDate.setDate(
-      nextDate.getDate() +
-      engagement.nextDay
-    );
+    if (engagement.nextDay !== null) {
+      nextDate = new Date(applicationDate);
 
-    await updateEngagement(
+      nextDate.setDate(
+        nextDate.getDate() +
+        engagement.nextDay
+      );
+
+      nextDate =
+        nextDate.toLocaleDateString();
+    }
+
+    await updateFunction(
       sheetRow,
       engagement.stage,
-      nextDate.toLocaleDateString()
+      nextDate
     );
   }
+}
+
+async function checkEngagement(
+  sendEngagementMessage
+) {
+
+  await processRows(
+    await getApplications(),
+    5,
+    16,
+    17,
+    updateEngagement,
+    sendEngagementMessage
+  );
+
+  await processRows(
+    await getSuppliers(),
+    4,
+    12,
+    13,
+    updateSupplierEngagement,
+    sendEngagementMessage
+  );
+
+  await processRows(
+    await getTransports(),
+    4,
+    15,
+    16,
+    updateTransportEngagement,
+    sendEngagementMessage
+  );
+
+  await processRows(
+    await getWarehouses(),
+    4,
+    17,
+    18,
+    updateWarehouseEngagement,
+    sendEngagementMessage
+  );
 }
 
 module.exports = {
