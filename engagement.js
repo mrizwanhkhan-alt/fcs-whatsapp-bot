ad380bf72f43715ccbfe636f588925464ffc9e4f
mrizwanhkhan-alt <mrizwanhkhan@gmail.com>
2026-08-07 15:35:39 +0400

    Update engagement.js

--- engagement.js
+++ engagement.js
@@ -1,12 +1,2 @@
-const engagementPlan = [
-  {
-    day: 0,
-    stage: "Application Submitted",
-    nextDay: 15,
-    message: "..."
-  }
-];
-
-module.exports = {
-  engagementPlan
-};
+const engagementPlan = [{ day: 15, stage: "15 Day Follow Up", nextDay: 15, message: "Your FCS Express Franchise Application is under review. Our Franchise Development Team will contact you when there is an update. Thank you for your patience." }];
+module.exports = { engagementPlan };
